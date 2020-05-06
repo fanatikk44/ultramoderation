0c5a44ab90ab2040ce9467adb4f068d41d5d7da3
fanadev <fanadev@git.com>
2020-05-06 07:05:48 +0200

    COMMIT #1

---
+++ [SPIGOT] UltraModération/src/fr/ultramoderation/UltraModeration.java
@@ -0,0 +1,15 @@
+package fr.ultramoderation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltraModeration extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
